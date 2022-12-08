b88affe3b4ede89eb4001e00a8445638828bc37f
mmann1123 <mmann1123@gmail.com>
2022-12-08 13:00:08 -0500

    adding quarterly image download

--- 0_ee_image_dowload.py
+++ 0_ee_image_dowload.py
@@ -1,6 +1,6 @@
 # use geepy environment, run earthengine authenticate in commandline first
 #%%
-#requires https://cloud.google.com/sdk/docs/install
+# requires https://cloud.google.com/sdk/docs/install
 # and https://developers.google.com/earth-engine/guides/python_install-conda
 
 
@@ -19,11 +19,11 @@ from geetools import ui, cloud_mask, batch
 # ## Define an ImageCollection
 site = ee.Geometry.Polygon(
     [
-        [34.291442183768666,-6.361841661400507],
-        [36.543639449393666,-6.361841661400507],
-        [36.543639449393666,-4.07537105824348],
-        [34.291442183768666,-4.07537105824348],
-        [34.291442183768666,-6.361841661400507],
+        [34.291442183768666, -6.361841661400507],
+        [36.543639449393666, -6.361841661400507],
+        [36.543639449393666, -4.07537105824348],
+        [34.291442183768666, -4.07537105824348],
+        [34.291442183768666, -6.361841661400507],
     ]
 )
 
@@ -38,22 +38,65 @@ region = site
 # extra = dict(sat="Sen_TOA")
 CLOUD_FILTER = 75
 
-#%%
+
+#%% QUARTERLY COMPOSITES
+
+q_finished = []
 for year in list(range(2021, 2023)):
     for month in list(range(1, 13)):
 
-        print("year ", str(year), " month ", str(month))
         dt = pendulum.datetime(year, month, 1)
-        # start = pendulum.datetime(2017, 1, 1)
-        # end = pendulum.datetime(2021, 12, 31)
+        # avoid repeating same quarter
+        yq = f"{year}_{dt.quarter}"
+        if yq in q_finished:
+            # print('skipping')
+            continue
+        else:
+            # print(f"appending {year}_{dt.quarter}")
+            q_finished.append(f"{year}_{dt.quarter}")
 
-        # period = pendulum.period(start, end)
-        # for dt in period.range("months", 3):
+        print(f"Year: {year} Quarter: {dt.quarter}")
 
         collection = get_s2A_SR_sr_cld_col(
             site,
-            dt.start_of("month").to_datetime_string()[0:10],
-            dt.end_of("month").to_datetime_string()[0:10],
+            dt.first_of("quarter").strftime(r"%Y-%m-%d"),
+            dt.last_of("quarter").strftime(r"%Y-%m-%d"),
+            CLOUD_FILTER=CLOUD_FILTER,
+        )
+
+        s2_sr = (
+            collection.map(add_cld_shdw_mask)
+            .map(apply_cld_shdw_mask)
+            .select(bands)
+            .median()
+        )
+        s2_sr = geetools.batch.utils.convertDataType("uint32")(s2_sr)
+        # eprint(s2_sr)
+
+        # # export clipped result in Tiff
+        crs = "EPSG:4326"
+
+        img_name = f"S2_SR_{year}_Q{str(dt.quarter).zfill(2)}"
+        export_config = {
+            "scale": scale,
+            "maxPixels": 50000000000,
+            "driveFolder": folder,
+            "region": site,
+        }
+        task = ee.batch.Export.image(s2_sr, img_name, export_config)
+        task.start()
+
+#%% MONTHLY COMPOSITES
+for year in list(range(2021, 2023)):
+    for month in list(range(1, 13, 4)):
+
+        print("year ", str(year), " month ", str(month))
+        dt = pendulum.datetime(year, month, 1)
+
+        collection = get_s2A_SR_sr_cld_col(
+            site,
+            dt.start_of("month").strftime(r"%Y-%m-%d"),
+            dt.end_of("month").strftime(r"%Y-%m-%d"),
             CLOUD_FILTER=CLOUD_FILTER,
         )
 
