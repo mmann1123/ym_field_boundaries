201fabeb367b6820be6e6d0d742afb210473bf7e
mmann1123 <mmann1123@gmail.com>
2023-03-06 11:30:24 -0500

    push to aws

---
+++ aws_cultionet_working_install.sh
@@ -0,0 +1,71 @@
+# aws  linux  
+# AMI Deep learning AMI GPU PyTorch 1.13.1
+# G3 instance but  P3, P3dn, P4d, G5, G4dn also work
+
+
+conda config --set auto_activate_base true
+source activate pytorch
+
+sudo apt update -y && \
+    sudo apt upgrade -y && \
+    sudo apt install software-properties-common -y && \
+    sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y && \
+    sudo apt update -y && \
+    sudo apt install \
+    build-essential \
+    python3.8 \
+    python3-pip \
+    libgeos++-dev \
+    libgeos-3.8.0 \
+    libgeos-c1v5 \
+    libgeos-dev \
+    libgeos-doc \
+    libspatialindex-dev \
+    g++ \
+    libgdal-dev \
+    gdal-bin \
+    libproj-dev \
+    libspatialindex-dev \
+    geotiff-bin \
+    libgl1 \
+    pip \
+    git -y && \
+    sudo apt upgrade -y 
+ 
+
+export CPLUS_INCLUDE_PATH="/usr/include/gdal"
+export C_INCLUDE_PATH="/usr/include/gdal"
+export LD_LIBRARY_PATH="/usr/local/lib"
+
+
+pip install testresources
+pip install -U pip setuptools wheel
+pip install -U --no-cache-dir "setuptools<=58.*"
+pip install -U --no-cache-dir cython>=0.29.*
+pip install -U --no-cache-dir "numpy>=1.21.0,<1.24"
+
+# Install PyTorch Geometric and its dependencies
+pip install \
+    torch \
+    torchvision \
+    torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
+
+TORCH_VERSION=`(python -c "import torch;print(torch.__version__)")` &&
+    pip install \
+    torch-scatter \
+    torch-sparse \
+    torch-cluster \
+    torch-spline-conv \
+    torch-geometric -f https://data.pyg.org/whl/torch-${TORCH_VERSION}.html
+
+GDAL_VERSION=$(gdal-config --version | awk -F'[.]' '{print $1"."$2"."$3}') && \
+    pip install GDAL==$GDAL_VERSION --no-binary=gdal
+
+# Install cultionet
+pip install --user cultionet@git+https://github.com/jgrss/cultionet.git
+git clone https://github.com/jgrss/cultionet.git
+pip install testfixtures
+cd cultionet/tests
+python -m unittest
+
+
