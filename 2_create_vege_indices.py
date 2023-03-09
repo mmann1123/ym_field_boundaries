84ff6d3b391c5c7d8ed875e6e47f584021c25abd
mmann1123 <mmann1123@gmail.com>
2023-03-09 09:01:34 -0500

    create other  bands

--- 2_create_vege_indices.py
+++ 2_create_vege_indices.py
@@ -2,17 +2,18 @@
 import geowombat as gw
 from glob import glob
 import os
-os.chdir('/home/mmann1123/extra_space/Dropbox/TZ_field_boundaries/training_data/raw_images_S2')
+os.chdir('/home/mmann1123/extra_space/Dropbox/TZ_field_boundaries/raw_images_S2')
 files = glob('**/**.tif', recursive=True)
-
-#%%
-! pwd
+ 
 
 #%%
 for file in files:
     # create directories for evi and tasseled cap
     names = os.path.split(file)
     ! mkdir -p {names[0]}/evi
+    ! mkdir -p {names[0]}/red
+    ! mkdir -p {names[0]}/green
+    ! mkdir -p {names[0]}/blue
 
     # read in the file and calculate the indices
     with gw.config.update(sensor='bgrn' ):
@@ -22,7 +23,17 @@ for file in files:
             # calculate evi
             evi = ds.gw.evi()
             evi.gw.save(
-            os.path.join(names[0],'evi',names[1]), num_workers=4 )
+                os.path.join(names[0],'evi',names[1]), num_workers=4 )
+            red = ds.sel(band='red')
+            red.gw.save(
+                os.path.join(names[0],'red',names[1]), num_workers=4, overwrite=True )
+            green = ds.sel(band='green')
+            green.gw.save(
+                os.path.join(names[0],'green',names[1]), num_workers=4, overwrite=True )
+            blue = ds.sel(band='blue')
+            blue.gw.save(
+                os.path.join(names[0],'blue',names[1]), num_workers=4, overwrite=True )
+            
             # moved to time_series_vars folder
 
 # %%
