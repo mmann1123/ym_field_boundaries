1a55b3ef91055102feede1e94dd034cc4461d542
mmann1123 <mmann1123@gmail.com>
2023-04-11 16:58:36 +0000

    mount efs

--- aws_cultionet_pyenv_working?.sh
+++ aws_cultionet_pyenv_working?.sh
@@ -161,3 +161,8 @@ git clone https://github.com/jgrss/cultionet.git
 pip install testfixtures pytest
 cd cultionet/tests
 pytest -vv
+
+
+# mount flexible storage
+mkdir efs -p
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-064b6369b27fcf25f.efs.us-east-1.amazonaws.com:/ efs
