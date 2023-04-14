edc1d1858f1316be48d122567428f7768548a18d
mmann1123 <mmann1123@gmail.com>
2023-04-14 14:35:11 -0400

    clean files

--- aws_mann_cultionet_v2_install.sh
+++ aws_mann_cultionet_v2_install.sh
@@ -1,4 +1,4 @@
-# works but only with docker
+# works but only with docker from within AWS instance using pytorch_lighting 1.95
 
 
 # Ubuntu Linux 20.04
@@ -89,7 +89,7 @@ RUN pip install --user cultionet@git+https://github.com/jgrss/cultionet.git
 RUN git clone https://github.com/jgrss/cultionet.git
 CMD /bin/bash
 
-# try running on AWS instance?? 
+# try running on AWS instance
 # cp GWU_deep_learning_cultionet dockerfile
 # docker build -t cultionet .
 # docker images -a 
@@ -97,112 +97,5 @@ CMD /bin/bash
 # cd cultionet/tests
 # pip install -U pytest
 # pytest -vv
-
-cultionet create \
-    --transforms {fliplr,flipud,rot90,rot180,rot270,tswarp,tsnoise,tsdrift,roll,gaussian,saltpepper,speckle} \
-    --res 0.00008983152841195214829 \
-    --project-path . \
-    --config-file config.yml \
-    --crop-column class \
-    --grid-size 99 99 \
-    --max-crop-class 1 \
-    --image-date-format %Y%j \
-    --feature-pattern {region}/{image_vi} \
-    --start-date 05-01 \
-    --end-date 05-01
-
-#cutltionet train --expected-dim 4 --expected-height 100 --expected-width 100 --delete-mismatches --recalc-zscores {other args ...}
-
-cultionet train --project-path . \
-    --delete-mismatches \
-    --expected-dim 4 \
-    --expected-width 99 \
-    --expected-height 100 \
-    --recalc-zscores\
-    --val-frac 0.2 \
-    --random-seed 500 \
-    --batch-size 4 \
-    --epochs 30 \
-    --filters 32 \
-    --device cpu \
-    --patience 5 \
-    --learning-rate 0.001 \
-    --reset-model
-
-# try running on AWS instance?? 
-# cp GWU_deep_learning_cultionet dockerfile
-# docker build -t cultionet .
-# docker images -a 
-# docker run -v /home/ubuntu:/home  -it cultionet
-# cd cultionet/tests
-# pip install -U pytest
-#pytest -vv
-
-
-# sudo apt update -y && \
-#     sudo apt upgrade -y && \
-#     sudo apt install software-properties-common -y && \
-#     sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y && \
-#     sudo apt update -y && \
-#     sudo apt install \
-#     build-essential \
-#     python3.8 \
-#     python3-pip \
-#     libgeos++-dev \
-#     libgeos-3.8.0 \
-#     libgeos-c1v5 \
-#     libgeos-dev \
-#     libgeos-doc \
-#     libspatialindex-dev \
-#     g++ \
-#     libgdal-dev \
-#     gdal-bin \
-#     libproj-dev \
-#     libspatialindex-dev \
-#     geotiff-bin \
-#     libgl1 \
-#     pip \
-#     git -y && \
-#     sudo apt upgrade -y 
  
-
-# export CPLUS_INCLUDE_PATH="/usr/include/gdal"
-# export C_INCLUDE_PATH="/usr/include/gdal"
-# export LD_LIBRARY_PATH="/usr/local/lib"
-
-
-# pip install testresources
-# pip install -U pip setuptools wheel
-# pip install -U --no-cache-dir "setuptools<=58.*"
-# pip install -U --no-cache-dir cython>=0.29.*
-# pip install -U --no-cache-dir "numpy>=1.21.0,<1.24"
-# pip install intel-openmp
-
-# # Install PyTorch Geometric and its dependencies
-# pip install \
-#     torch \
-#     torchvision \
-#     torchaudio  
-
-# TORCH_VERSION=`(python -c "import torch;print(torch.__version__)")` &&
-#     pip install \
-#     torch-scatter \
-#     torch-sparse \
-#     torch-cluster \
-#     torch-spline-conv \
-#     torch-geometric -f https://data.pyg.org/whl/torch-${TORCH_VERSION}.html
-
-# GDAL_VERSION=$(gdal-config --version | awk -F'[.]' '{print $1"."$2"."$3}') && \
-#     pip install GDAL==$GDAL_VERSION --no-binary=gdal
-
-# # Install cultionet
-# pip install --user cultionet@git+https://github.com/jgrss/cultionet.git
-# git clone https://github.com/jgrss/cultionet.git
-# pip install testfixtures
-# sudo apt install python-pytest -y
-# cd cultionet/tests
-# pytest -vv
-
-# # mount flexible storage
-# # mkdir efs -p
-# # sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-064b6369b27fcf25f.efs.us-east-1.amazonaws.com:/ efs
+# see 3_cultionet_exe.sh for how to run cultionet
